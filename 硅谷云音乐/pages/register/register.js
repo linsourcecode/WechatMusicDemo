43f187bbb208cea5e0c73d9c609825ac66a4f090
WIN-J5TK1O2D7JK\Administrator <1679569188@qq.com>
2021-05-14 23:17:05 +0800

    成功获取信息

--- 硅谷云音乐/pages/register/register.js
+++ 硅谷云音乐/pages/register/register.js
@@ -1,29 +1,21 @@
-/**
-  作者: Created by zhiyongzaixian
-  说明: 登录流程
-  1. 收集表单项数据
-  2. 前端验证
-    1) 验证用户信息(账号，密码)是否合法
-    2) 前端验证不通过就提示用户，不需要发请求给后端
-    3) 前端验证通过了，发请求(携带账号, 密码)给服务器端
-  3. 后端验证
-    1) 验证用户是否存在
-    2) 用户不存在直接返回，告诉前端用户不存在
-    3) 用户存在需要验证密码是否正确
-    4) 密码不正确返回给前端提示密码不正确
-    5) 密码正确返回给前端数据，提示用户登录成功(会携带用户的相关信息)
-*/
 import request from '../../utils/request'
 Page({
 
   /**
    * 页面的初始数据
    */
-  data: {
-    phone: '', // 手机号
-    password: '' // 用户密码
-  },
+  globalData: {
+    hasLogin: false,
+    phone: ''
 
+  },
+  data: {
+    text: '获取验证码', //按钮文字
+    phone: '', // 手机号
+    password: '', // 用户密码
+    captcha:'',
+    nickname:''
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -39,9 +31,29 @@ Page({
     this.setData({
       [type]: event.detail.value
     })
+    this.globalData.phone = this.data.phone
+    console.log(this.globalData.phone)
   },
-  
-  
+  async getChapt(){
+    let phone = this.data.phone;
+    console.log("全局变量",this.globalData.phone)
+    let result = await request('/captcha/sent',{phone})
+    if(result.code==200){
+      wx.showToast({
+        title: '信息发送成功',
+        icon: 'none'
+      })
+
+    }else{
+      wx.showToast({
+        title: '信息发送失败',
+        icon: 'none'
+      })
+    }
+
+  },
+
+
   // 登录的回调
   async login(){
     // 1. 收集表单项数据
@@ -53,7 +65,7 @@ Page({
     *   2. 手机号格式不正确
     *   3. 手机号格式正确，验证通过
     * */
-    
+
     if(!phone){
       // 提示用户
       wx.showToast({
@@ -71,7 +83,7 @@ Page({
       })
       return;
     }
-    
+
     if(!password){
       wx.showToast({
         title: '密码不能为空',
@@ -79,17 +91,18 @@ Page({
       })
       return;
     }
-  
+
+
     // 后端验证
-    let result = await request('/login/cellphone', {phone, password, isLogin: true})
+    let result = await request('/register/cellphone', {phone, password, captcha, nickname, isLogin: true})
     if(result.code === 200){ // 登录成功
       wx.showToast({
         title: '登录成功'
       })
       // 将用户的信息存储至本地
       wx.setStorageSync('userInfo', JSON.stringify(result.profile))
-      
-      
+
+
       // 跳转至个人中心personal页面
       wx.reLaunch({
 
@@ -117,7 +130,7 @@ Page({
       url: '/pages/register/register'
     })
   },
-  
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
